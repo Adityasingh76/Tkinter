6c0527389ecc6783f3e1fde66073632c67cf383d
Adityasingh76 <77424628+Adityasingh76@users.noreply.github.com>
2021-07-09 17:23:07 +0530

    Delete test.py

--- 2048/test.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-window = tk.Tk()
-window.wm_attributes("-transparentcolor","#ffffff")
-a = tk.Frame(window,bg = "#000000")
-a.grid(row = 0,column = 0)
-b = tk.Label(a,width = 5,height = 5,bg = "#ffffff")
-b.grid(row = 0,column = 0,padx = 30,pady = 30)
-window.mainloop()
